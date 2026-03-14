f638b405495c5bbac83ac0bd988434e788b39693
Jannat63 <ajbmix63@gmail.com>
2026-03-14 12:01:42 +0600

    Post Grid Upgrade

--- components/PostGrid.tsx
+++ components/PostGrid.tsx
@@ -30,7 +30,7 @@ export default function PostGrid({ posts, showHeader = true }: any) {
 
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
 
-        {posts.map((post: any) => (
+        {posts.slice(0, 6).map((post: any) => (
           <BlogCard
             key={post.slug}
             title={post.title}
