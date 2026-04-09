e1867cd383e4287919e793d74843e68d5f2d14d1
Jannat63 <ajbmix63@gmail.com>
2026-04-09 08:59:32 +0600

    Home page

--- app/page.tsx
+++ app/page.tsx
@@ -155,9 +155,9 @@ export default async function Home() {
 
       <FeaturedPosts posts={featured} />
 
-      <TrendingPosts posts={sortedPosts} />
+      {/* <TrendingPosts posts={sortedPosts} /> */}
 
-      <PostGrid posts={sortedPosts.slice(8, 14)} />
+      {/* <PostGrid posts={sortedPosts.slice(8, 14)} /> */}
 
 
         {/* FEATURED STORY */}
